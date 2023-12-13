7d38870967bad2340b66c2a1b508ffaaa3cfce9d
ItzzM33 <max986532@gmail.com>
2023-12-14 00:32:17 +0200

    M33

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+
+var qoute =[`“Be yourself; everyone else is already taken.”
+― Oscar Wilde`,
+`“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”
+― Marilyn Monroe`,
+`“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”
+― Albert Einstein`,
+`“So many books, so little time.”
+― Frank Zappa`,
+`“A room without books is like a body without a soul.”
+― Marcus Tullius Cicero`,
+`“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”
+― Bernard M. Baruch`]
+function typeQoute(){
+    document.getElementById('outPut').innerHTML=qoute[Math.floor(Math.random()*qoute.length)]
+
+}
+
+
+
+
+
+
+
+
+
+alert
